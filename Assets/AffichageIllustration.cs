3cad64890e3c94e25913e60dc76b415c3fcbbeff
FranciaMMI <francia.bidzimou@mmibordeaux.com>
2022-12-06 00:02:06 +0100

    new version

--- Assets/AffichageIllustration.cs
+++ Assets/AffichageIllustration.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AffichageIllustration : MonoBehaviour
 {
 
     [SerializeField] private List<GameObject> illustration;
-    public int nbIllustration = 0;
-    public int nbKeyDown = 0;
+    private int nbIllustration = 0;
+    public string LevelToLoad;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +22,13 @@ public class AffichageIllustration : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         { 
         ResetIllustrationDisplay();
-        }
 
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene(LevelToLoad);
+
+        }
 
     }
 
